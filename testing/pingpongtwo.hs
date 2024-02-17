3975cd9160eefc689e07821279a9104a8cb63e56
alexkim715 <doyoonki@buffalo.edu>
2024-02-17 17:59:01 -0500

    got rid of useless func, added printing for testing

--- testing/pingpongtwo.hs
+++ testing/pingpongtwo.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BlockArguments #-}
+
 import Control.Concurrent (threadDelay)
 import Control.Monad.Choreo
 
@@ -11,14 +13,8 @@ pingpong ::
   Choreo IO (Response @ "ping")
 pingpong request state = do
   request' <- (ping, request) ~> server
-  response <- server 'locally' \un -> delayTime (un request') (un state)
+  response <- server 'locally' \un -> return Nothing
   (server, response) ~> ping
-
--- keeping the delayTime because not sure about line 10.
-delayTime :: Request -> IORef State -> IO Response
-
-threadDelay 2000000
-return
 
 mainChoreo :: Int -> Choreo IO ()
 mainChoreo state = loop state -- start loop with i = 5
@@ -26,6 +22,7 @@ mainChoreo state = loop state -- start loop with i = 5
     loop :: Int -> Choreo IO ()
     loop 0 = terminate -- when state = 0, terminate the loop
     loop state = do
+      liftIO $ putStrLn $ "i: " ++ show (state - 1)
       request <- ping `locally` \_ -> readRequest
       response <- pingpong request state
       loop (state - 1) -- recursive call where state is decremented
