d99ce02b7b04d5cdc83b4c77314376c78d909de9
alexkim715 <doyoonki@buffalo.edu>
2024-04-18 16:20:06 -0400

    First working dot generator (still needs testing)

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,21 @@
-open Ast.Interface
 open Parsing.Interface
+open Pirouettedot.Interface
+
+let dot_to_file filename dot_code =
+  let file_oc = open_out filename in
+  Printf.fprintf file_oc "%s" dot_code;
+  close_out file_oc
 
 let () =
-  if Array.length Sys.argv < 2 then (
-    print_endline "Usage: <executable> <sourcefile>";
-    exit 1)
+  if Array.length Sys.argv < 3 then (
+    print_endline "Usage: <executable> <sourcefile> <destfile>";
+    exit 1) 
   else
-    let filename = Sys.argv.(1) in
-    let file_ic = open_in filename in
+    let in_filename = Sys.argv.(1) in
+    let file_ic = open_in in_filename in
     let lexbuf = Lexing.from_channel file_ic in
     let program = parse_program lexbuf in
-    print_endline (dump_choreo_ast program);
-    close_in file_ic
+    close_in file_ic;
+    let dot_code = generate_dot_code program in
+    let out_filename = Sys.argv.(2) ^ ".dot" in
+    dot_to_file out_filename dot_code
